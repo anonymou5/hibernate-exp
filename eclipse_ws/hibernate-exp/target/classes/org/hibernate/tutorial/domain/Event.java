e0255061f33c3913067d9c09a1798e573569dbdb
Jonathan Ferland <jmferland@gmail.com>
2010-09-05 11:34:28 -0600

    first commit

---
+++ eclipse_ws/hibernate-exp/target/classes/org/hibernate/tutorial/domain/Event.java
@@ -0,0 +1,14 @@
+package org.hibernate.tutorial.domain;
+
+import java.util.Date;
+
+public class Event {
+	private Long id;
+	private String title;
+	private Date date;
+
+	public Event() {
+	}
+	
+	
+}
